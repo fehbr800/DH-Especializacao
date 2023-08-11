21aca3e235546cfd25db3d18b92d4ea37bc7fab8
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-08-10 20:24:04 -0400

    hook-form

--- Ignite-Rocktseat/Aulas/aula-02-timer/src/pages/Home/index.tsx
+++ Ignite-Rocktseat/Aulas/aula-02-timer/src/pages/Home/index.tsx
@@ -1,16 +1,79 @@
-import{ Play} from '@phosphor-icons/react'
-import { CountDownContainer, FormContainer, HomeContainer, Separator } from './styles';
+import { Play } from "@phosphor-icons/react";
+import { useForm } from "react-hook-form"
+
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartComponentButton,
+  TaskInput,
+} from "./styles";
+
+
+
+
+// Formas de trabalhar com o formulário com React:
+// Controlled / uncontrolled 
+//Controlled: forma de manter a informação do usuário no estado em tempo real; Ex: Pequenos, formularios, e poucos inputs
+//Uncontrolled: busca a informação somente quando for preciso; Ex: Dashboards, varios inputs
 
 export function Home() {
+
+ const {register, handleSubmit, watch} = useForm()
+
+/*
+function register(name: string){
+ela   return{
+  onChange:() => void,
+  onBlur:() => void,
+  onFocus: () => void,
+  }
+}
+
+*/
+
+  function handleCreateNewCycle(data){
+ console.log(data)
+
+
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task;
+  
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="text" id="task" />
+          <TaskInput
+            list="task-suggestions"
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+           
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1 " />
+            <option value="Projeto 2 " />
+            <option value="Projeto 3 " />
+            <option value="Projeto 4 " />
+          </datalist>
 
           <label htmlFor="">durante</label>
-          <input type="number" id="minutsAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount',{valueAsNumber: true})}
+          />
           <span>minutos.</span>
         </FormContainer>
 
@@ -21,7 +84,9 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountDownContainer>
-        <button type="submit">Começar <Play size={24}/> </button>
+        <StartComponentButton disabled={isSubmitDisabled}  type="submit">
+          Começar <Play size={24} />{" "}
+        </StartComponentButton>
       </form>
     </HomeContainer>
   );
--- Ignite-Rocktseat/Aulas/aula-02-timer/src/pages/Home/styles.ts
+++ Ignite-Rocktseat/Aulas/aula-02-timer/src/pages/Home/styles.ts
@@ -36,13 +36,49 @@ flex-wrap:wrap;
 
 `
 
+
+const BaseInput = styled.input`
+ background: transparent;
+ height:2.5rem;
+ border: 0;
+ border-bottom: 2px solid ${props => props.theme["gray-500"]};
+ font-weight: bold;
+ font-size: 1.125rem;
+ padding: 0 0.5rem;
+ color:  ${props => props.theme["gray-100"]};
+
+&:focus{
+ box-shadow: none;
+ border-color: ${props => props.theme["green-500"]}
+};
+
+ &::placeholder{
+    color: ${props => props.theme["gray-500"]};
+ };
+
+`
+
+export const TaskInput = styled(BaseInput)`
+flex:1;
+ 
+&::-webkit-calendar-picker-indicator{
+    display: none !important;
+};
+
+`;
+
+
+export const MinutesAmountInput = styled(BaseInput)`
+   width:4rem;
+
+`;
+
 export const CountDownContainer = styled.div `
 
 font-family: 'Roboto Mono', monospace;
 font-size: 10rem;
 line-height: 8rem;
 color: ${props => props.theme["gray-100"]};
-
 display:flex;
 gap: 1rem;
 
@@ -63,5 +99,36 @@ width:4rem;
 overflow:hidden;
 display:flex;
 justify-content:center;
+
+`
+
+export const StartComponentButton = styled.button`
+
+width:100%;
+border:none;
+padding:1rem;
+
+border-radius:8px;
+
+display:flex;
+align-items: center;
+justify-content: center;
+
+gap:0.5rem;
+font-weight:bold;
+cursor:pointer;
+background: ${props => props.theme["green-500"]};
+color: ${props => props.theme["gray-100"]};
+
+&:disabled{
+    opacity:0.7;
+    cursor:not-allowed;
+};
+
+&:not(:disabled):hover{
+    background: ${props => props.theme["green-700"]};
+}
+
+
 
 `
