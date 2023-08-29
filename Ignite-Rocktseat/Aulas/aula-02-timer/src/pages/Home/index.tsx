136f0dedaf1d54c660dbd1477bd0bdd44e209339
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-08-28 20:05:16 -0400

    encerrando ciclo

--- Ignite-Rocktseat/Aulas/aula-02-timer/src/pages/Home/index.tsx
+++ Ignite-Rocktseat/Aulas/aula-02-timer/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from "@phosphor-icons/react";
+import { HandPalm, Play } from "@phosphor-icons/react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
@@ -11,6 +11,7 @@ import {
   MinutesAmountInput,
   Separator,
   StartComponentButton,
+  StopComponentButton,
   TaskInput,
 } from "./styles";
 import { useEffect, useState } from "react";
@@ -32,6 +33,8 @@ interface Cycle {
   task: string;
   minutesAmount: number;
   startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
 }
 
 export function Home() {
@@ -56,20 +59,39 @@ ela   return{
 }
 
 */
+
   const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   useEffect(() => {
-    let interval:number;
+    let interval: number;
     if (activeCycle) {
       interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
         );
+        if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id == activeCycleId) {
+                return { ...cycle, finishedDate: new Date() };
+              } else {
+                return cycle;
+              }
+            })
+          )
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference);
+        }
       }, 1000);
     }
-    return ()=>{
-      clearInterval(interval)
-    }
-  }, [activeCycle]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle, totalSeconds]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
@@ -83,12 +105,11 @@ ela   return{
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
-    setAmountSecondsPassed(0)
+    setAmountSecondsPassed(0);
 
     reset();
   }
 
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
   const minutesAmount = Math.floor(currentSeconds / 60);
@@ -97,9 +118,22 @@ ela   return{
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
 
-  useEffect(()=>{
-    document.title = `${minutes}:${seconds}`
-  },[minutes,seconds, activeCycle])
+  useEffect(() => {
+    document.title = `${minutes}:${seconds}`;
+  }, [minutes, seconds, activeCycle]);
+
+  function handleInterruptCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id == activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
+    setActiveCycleId(null);
+  }
 
   const task = watch("task");
   const isSubmitDisabled = !task;
@@ -114,6 +148,7 @@ ela   return{
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
             {...register("task")}
           />
 
@@ -132,6 +167,7 @@ ela   return{
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register("minutesAmount", { valueAsNumber: true })}
           />
           <span>minutos.</span>
@@ -144,9 +180,15 @@ ela   return{
           <span>{seconds[0]} </span>
           <span>{seconds[1]} </span>
         </CountDownContainer>
-        <StartComponentButton disabled={isSubmitDisabled} type="submit">
-          Começar <Play size={24} />{" "}
-        </StartComponentButton>
+        {activeCycle ? (
+          <StopComponentButton onClick={handleInterruptCycle} type="button">
+            Interromper <HandPalm size={24} />{" "}
+          </StopComponentButton>
+        ) : (
+          <StartComponentButton disabled={isSubmitDisabled} type="submit">
+            Começar <Play size={24} />{" "}
+          </StartComponentButton>
+        )}
       </form>
     </HomeContainer>
   );
