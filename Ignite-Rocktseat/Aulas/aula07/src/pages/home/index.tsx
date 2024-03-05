1a3e6c80350f49ef274cd539b068b2bd4ff707b1
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-03-05 19:03:04 -0400

    form

--- Ignite-Rocktseat/Aulas/aula07/src/pages/home/index.tsx
+++ Ignite-Rocktseat/Aulas/aula07/src/pages/home/index.tsx
@@ -1,9 +1,31 @@
-import { Heading } from '@ignite-ui/react'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+import previewImage from '@/assets/app-preview.png'
+import Image from 'next/image'
+import { ClaimUsernameForm } from './components/ClainUserNameForm'
 
 export default function Home() {
   return (
-    <>
-      <Heading as="h1">Ola mundo</Heading>
-    </>
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="preview image"
+        />
+      </Preview>
+    </Container>
   )
 }
--- Ignite-Rocktseat/Aulas/aula07/src/pages/home/styles.ts
+++ Ignite-Rocktseat/Aulas/aula07/src/pages/home/styles.ts
@@ -1,7 +1,35 @@
-import { styled } from '@ignite-ui/react'
+import { Heading, Text, styled } from '@ignite-ui/react'
 
-export const Container = styled('div', {})
+export const Container = styled('div', {
+  display: 'flex',
+  gap: '$20',
+  marginLeft: 'auto',
+  alignItems: 'center',
+  height: '100vh',
+  maxWidth: 'calc(100vw - (100vw - 1240px)/2)',
+})
 
-export const Hero = styled('div', {})
+export const Hero = styled('div', {
+  maxWidth: '480px',
+  padding: '0 $10',
 
-export const Preview = styled('div', {})
+  [`${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRigth: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
