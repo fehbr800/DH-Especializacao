b3f52dc9a09b9b406519db985ad64523a3ba3b5a
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-03-05 20:55:26 -0400

    fazendo validações

--- Ignite-Rocktseat/Aulas/aula07/src/pages/home/components/ClainUserNameForm/index.tsx
+++ Ignite-Rocktseat/Aulas/aula07/src/pages/home/components/ClainUserNameForm/index.tsx
@@ -1,15 +1,55 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const ClaimUserNameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Digite um nome valido' })
+    .regex(/^([a-z\\-]+)$/i)
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUserNameFormData = z.infer<typeof ClaimUserNameFormSchema>
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUserNameFormData>({
+    resolver: zodResolver(ClaimUserNameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUserNameFormData) {
+    console.log(data)
+  }
   return (
-    <Form>
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu usuario" />
-      <Button size="sm" type="submit">
-        Reservar usuário
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuario'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
