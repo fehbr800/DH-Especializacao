3e260a077897bb1b57487da635bd588e6616dace
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-04-12 19:25:46 -0400

    fazendo logica para bloqueio dos dias

--- Ignite-Rocktseat/Aulas/aula07/src/pages/_app.page.tsx
+++ Ignite-Rocktseat/Aulas/aula07/src/pages/_app.page.tsx
@@ -1,15 +1,19 @@
 import { globalStyles } from '@/styles/global'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
-
+import '../lib/dayjs'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 globalStyles()
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
