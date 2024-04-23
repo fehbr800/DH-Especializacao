9f484cd1316159cc4d220856c2b69ffc10bdb12b
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-04-23 19:31:06 -0400

    fazendo algumas migrações e ajustes finais

--- Ignite-Rocktseat/Aulas/aula07/src/pages/home/index.tsx
+++ Ignite-Rocktseat/Aulas/aula07/src/pages/home/index.tsx
@@ -3,29 +3,36 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '@/assets/app-preview.png'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClainUserNameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading as="h1" size="4xl">
-          Agendamento descomplicado
-        </Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100}
-          priority
-          alt="preview image"
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="preview image"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
