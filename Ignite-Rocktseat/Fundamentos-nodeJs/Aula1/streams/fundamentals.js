b3be3ee5f68bdbfd712f438b96a3249969c2a712
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-05-08 19:27:05 -0400

    streams

--- Ignite-Rocktseat/Fundamentos-nodeJs/Aula1/streams/fundamentals.js
+++ Ignite-Rocktseat/Fundamentos-nodeJs/Aula1/streams/fundamentals.js
@@ -12,3 +12,29 @@
 // 10mb/s -> 10.000
 
 // Readable Streams / Writable Streams
+
+
+// process.stdin.pipe(process.stdout)
+
+import {Readable} from 'node:stream'
+
+class OneToHundredStream extends Readable{
+    index = 1
+    _read(){
+        const i = this.index++
+
+        setTimeout(() => {
+            if (i > 100) {
+              this.push(null)
+            } else {
+              const buf = Buffer.from(String(`${i}\n`))
+      
+              this.push(buf)
+            }
+          }, 1000);
+        }
+      }
+
+
+new OneToHundredStream()
+.pipe(process.stdout)
