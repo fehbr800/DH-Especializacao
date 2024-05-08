688d77036d53d292ea672ae911470d7831c77d92
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-05-08 19:05:18 -0400

    iniciando com streams

--- Ignite-Rocktseat/Fundamentos-nodeJs/Aula1/src/server.js
+++ Ignite-Rocktseat/Fundamentos-nodeJs/Aula1/src/server.js
@@ -1,7 +1,52 @@
 import  http from'node:http';  
 
-const server = http.createServer((req,res)=>{
-    return res.end('Hello world!');
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banck-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
+
+const users = []
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req
+  
+    if (method === 'GET' && url === '/users') {
+      return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
+    }
+  
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id:1,
+            name: 'Matheus',
+            age: 25,
+        })
+        return res.writeHead(201).end()
+    }
+  
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
