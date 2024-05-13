56b6a24742877315936deee2b02127b169b1c245
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-05-13 19:26:22 -0400

    entendendo streams

--- Ignite-Rocktseat/Fundamentos-nodeJs/Aula1/src/server.js
+++ Ignite-Rocktseat/Fundamentos-nodeJs/Aula1/src/server.js
@@ -28,8 +28,22 @@ import  http from'node:http';
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     const { method, url } = req
+
+    const buffers = []
+
+    for await (const chunk of req){
+        buffers.push(chunk)
+    }
+
+try{
+   req.body =JSON.parse(Buffer.concat(buffers).toString())
+}catch{
+    req.body = null
+}
+
+
   
     if (method === 'GET' && url === '/users') {
       return res
@@ -38,10 +52,11 @@ const server = http.createServer((req, res) => {
     }
   
     if (method === 'POST' && url === '/users') {
+        const {name, email} = req.body
         users.push({
             id:1,
-            name: 'Matheus',
-            age: 25,
+            name,
+            email,
         })
         return res.writeHead(201).end()
     }
